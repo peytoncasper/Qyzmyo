e83e4a2e8eeef08f2e8761648cc45003a2f18d41
Son Pham <sonpa1993@gmail.com>
2015-03-28 12:27:52 -0500

    reloadQuestion

--- app.js
+++ app.js
@@ -32,49 +32,77 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$location', function ($scop
       }
     ];
     $scope.answers = [];
+    $scope.timeout = 5 * 1000;  // 5 secs per question
 
-    $(document).ready(function () {
-        // close nav bar if open
+  $(document).ready(function () {
+      // close nav bar if open
         $('#sidenav-overlay').trigger('click');
 
-        $('#preloader_div').hide();
+		$('#preloader_div').hide();
+	    // $('#main').hide();
+	    $('#score').hide();
 
-        /* *************** REGISTER *************** */
-        $('#register_btn').on('click', function () {
-            $('#register').fadeOut();
-            $('#register').hide();
-            $('#preloader_div').fadeIn();
+    /* *************** REGISTER *************** */
+    var reloadQuestion = function(questionObj) {
+      console.log('clicked');
+      var question = questionObj.name;
+      var answer1 = questionObj.answers[0];
+      var answer2 = questionObj.answers[1];
+      var answer3 = questionObj.answers[2];
+      $('#question').fadeOut();
+      $('#answer1').fadeOut();
+      $('#answer2').fadeOut();
+      $('#answer3').fadeOut();
+      $('#question').html(question);
+      $('#answer1').html(answer1);
+      $('#answer2').html(answer2);
+      $('#answer3').html(answer3);
+      $('#question').fadeIn();
+      $('#answer1').fadeIn();
+      $('#answer2').fadeIn();
+      $('#answer3').fadeIn();
+    };
 
-            // TODO
-            $('#preloader_div').fadeOut();
-            window.onkeyup = function (e) {
-                var key = e.keyCode ? e.keyCode : e.which;
+    // testing purpose
+    $('#next_btn').on('click', function() {
+      reloadQuestion($scope.questions[0]);
+    });
 
-                if (key == 65) {
-                    alert("hi");
-                    $($scope.answers).append({
-                        timestamp: new Date(),
-                        answer: 'a',
-                    });
-                    reloadQuestion($scope.questions[$($.scope.answers).length()]);
-                } else if (key == 66) {
-                    $($scope.answers).append({
-                        timestamp: new Date(),
-                        answer: 'b',
-                    });
-                    reloadQuestion($scope.questions[$($.scope.answers).length()]);
-                } else if (key == 67) {
-                    $($scope.answers).append({
-                        timestamp: new Date(),
-                        answer: 'c',
-                    });
-                    reloadQuestion($scope.questions[$($.scope.answers).length()]);
-                }
-            }
-            $.each($scope.questions, function (index, value) {
-                $("#main").append(value.name);
-            });
-        });
+    $('#register_btn').on('click', function() {
+      $('#register').fadeOut();
+      $('#register').hide();
+      $('#preloader_div').fadeIn();
+
+      // TODO
+      $('#preloader_div').fadeOut();
+      window.onkeyup = function (e) {
+          var key = e.keyCode ? e.keyCode : e.which;
+
+          if (key == 65) {
+              alert("hi");
+              $($scope.answers).append({
+                  timestamp: new Date(),
+                  answer: 'a',
+              });
+              reloadQuestion($scope.questions[$($.scope.answers).length()]);
+          } else if (key == 66) {
+              $($scope.answers).append({
+                  timestamp: new Date(),
+                  answer: 'b',
+              });
+              reloadQuestion($scope.questions[$($.scope.answers).length()]);
+          } else if (key == 67) {
+              $($scope.answers).append({
+                  timestamp: new Date(),
+                  answer: 'c',
+              });
+              reloadQuestion($scope.questions[$($.scope.answers).length()]);
+          }
+      }
+      $.each($scope.questions, function (index, value) {
+          $("#main").append(value.name);
+      });
+    });
 
         /* *************** MAIN *************** */
 
