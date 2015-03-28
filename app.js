df9b237ee6ed11c98dfccb2bb28ef97d56472da4
Peyton Casper <peytoncas@gmail.com>
2015-03-28 12:10:55 -0700

    Added in the key up event

--- app.js
+++ app.js
@@ -2,84 +2,103 @@
 
 var app = angular.module('app', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'home.html',
-    controller: 'HomeCtrl'
-  })
-  .when('/about', {
-    templateUrl: 'about.html',
-    controller: 'AboutCtrl'
-  })
-  .otherwise({
-    templateUrl: 'home.html',
-    controller: 'HomeCtrl'
-  });
-}]);
-
-app.controller('HomeCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
-  $rootScope.root = {
-    title: 'Home'
-  };
-
-  $scope.questions = [
-    {
-      name: '3 x 3 = ',
-      answers: ['3', '6', '9']
-    }, {
-        // TODO
-
-    }
-  ];
-
-
-  $(document).ready(function() {
-    // close nav bar if open
-    $('#sidenav-overlay').trigger('click');
-
-    $('#preloader_div').hide();
-
-    /* *************** REGISTER *************** */
-    $('#register_btn').on('click', function() {
-      $('#register').fadeOut();
-      $('#register').hide();
-      $('#preloader_div').fadeIn();
-
-      // TODO
-      $('#preloader_div').fadeOut();
-      
-      $(document).on('input', function ()
-      {
-          alert("Hi");
-      });
-      $.each($scope.questions, function (index, value) {
-          $("#main").append(value.name);
-      });
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        controller: 'HomeCtrl'
+    })
+    .when('/about', {
+        templateUrl: 'about.html',
+        controller: 'AboutCtrl'
+    })
+    .otherwise({
+        templateUrl: 'home.html',
+        controller: 'HomeCtrl'
     });
+}]);
 
-    /* *************** MAIN *************** */
+app.controller('HomeCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
+    $rootScope.root = {
+        title: 'Home'
+    };
+
+    $scope.questions = [
+      {
+          name: '3 x 3 = ',
+          answers: ['3', '6', '9']
+      }, {
+          // TODO
+
+      }
+    ];
+    $scope.answers = [];
+
+    $(document).ready(function () {
+        // close nav bar if open
+        $('#sidenav-overlay').trigger('click');
+
+        $('#preloader_div').hide();
+
+        /* *************** REGISTER *************** */
+        $('#register_btn').on('click', function () {
+            $('#register').fadeOut();
+            $('#register').hide();
+            $('#preloader_div').fadeIn();
+
+            // TODO
+            $('#preloader_div').fadeOut();
+            window.onkeyup = function (e) {
+                var key = e.keyCode ? e.keyCode : e.which;
+
+                if (key == 65) {
+                    alert("hi");
+                    $($scope.answers).append({
+                        timestamp: new Date(),
+                        answer: 'a',
+                    });
+                    reloadQuestion($scope.questions[$($.scope.answers).length()]);
+                } else if (key == 66) {
+                    $($scope.answers).append({
+                        timestamp: new Date(),
+                        answer: 'b',
+                    });
+                    reloadQuestion($scope.questions[$($.scope.answers).length()]);
+                } else if (key == 67) {
+                    $($scope.answers).append({
+                        timestamp: new Date(),
+                        answer: 'c',
+                    });
+                    reloadQuestion($scope.questions[$($.scope.answers).length()]);
+                }
+            }
+            $.each($scope.questions, function (index, value) {
+                $("#main").append(value.name);
+            });
+        });
+
+        /* *************** MAIN *************** */
 
 
 
 
 
-    /* *************** SCORE *************** */
+        /* *************** SCORE *************** */
 
 
 
 
 
-  });
+    });
 }]);
 
 
-app.controller('AboutCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-  $rootScope.root = {
-    title: 'About'
-  };
+app.controller('AboutCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $rootScope.root = {
+        title: 'About'
+    };
 
-  $(document).ready(function() {
-    // close nav bar if open
-    $('#sidenav-overlay').trigger('click');
-  });
+    $(document).ready(function () {
+        // close nav bar if open
+        $('#sidenav-overlay').trigger('click');
+    });
 }]);
