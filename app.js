b9c1a251d30f4cc31e8b8181d07226276c06dc16
Son Pham <sonpa1993@gmail.com>
2015-03-28 15:44:14 -0500

    fadeOutQuestion and fadeInQuestion

--- app.js
+++ app.js
@@ -67,24 +67,24 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$location', function ($scop
         $('#scoreboard').hide();
 
         /* *************** REGISTER *************** */
-        var reloadQuestion = function (questionObj) {
+        var fadeOutQuestion = function(answer, right) {
+        	// answer is 1, 2, or 3
+        	// right is true or falseg
+        }
+
+        var fadeInQuestion = function (questionObj) {
             var question = questionObj.name;
             var answer1 = questionObj.answers[0];
             var answer2 = questionObj.answers[1];
             var answer3 = questionObj.answers[2];
-            $('#question').fadeOut(function () {
-                $('#question').html(question);
-                $('#answer1').html(answer1);
-                $('#answer2').html(answer2);
-                $('#answer3').html(answer3);
-                $('#question').fadeIn();
-                $('#answer1').fadeIn();
-                $('#answer2').fadeIn();
-                $('#answer3').fadeIn();
-            });
-            $('#answer1').fadeOut();
-            $('#answer2').fadeOut();
-            $('#answer3').fadeOut();
+            $('#question').html(question);
+            $('#answer1').html(answer1);
+            $('#answer2').html(answer2);
+            $('#answer3').html(answer3);
+            $('#question').fadeIn();
+            $('#answer1').fadeIn();
+            $('#answer2').fadeIn();
+            $('#answer3').fadeIn();
         };
 
         $('#register_btn').on('click', function () {
@@ -92,7 +92,7 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$location', function ($scop
             $('#register').hide();
             $('#preloader_div').fadeIn();
 
-            reloadQuestion($scope.questions[0]);
+            fadeInQuestion($scope.questions[0]);
             $scope.name = $("#input-id").val();
             $('#preloader_div').fadeOut();
             $('#preloader_div').hide();
@@ -129,7 +129,7 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$location', function ($scop
                     $('#main').hide();
                     score();
                 } else {
-                    reloadQuestion($scope.questions[$scope.count]);
+                    fadeInQuestion($scope.questions[$scope.count]);
                 }
             };
         }
